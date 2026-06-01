aed1f38405d7d6ff2d6f0256dae0d244ffeebbb3
brau25-bit <brauliochaveza@gmail.com>
2026-06-01 15:27:27 -0600

    add: basic estructure

---
+++ src/parsers/manhwa18_parser.py
@@ -0,0 +1,10 @@
+
+class Manhwa18Parser:
+    async def search(series_name):
+        return "Hola"
+    
+    async def getSeries(series_url):
+        return
+    
+    async def getChapters(chapter_url):
+        return
