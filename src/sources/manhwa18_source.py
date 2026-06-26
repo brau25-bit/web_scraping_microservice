f46e9341de67802d7d69a4e1f9ed056812d9c4c6
brau25-bit <brauliochaveza@gmail.com>
2026-06-25 23:01:05 -0600

    add: uso de patron strategy para definir contratos

--- src/sources/manhwa18_source.py
+++ src/sources/manhwa18_source.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 from src.core.http_client import HTTPX
+from src.strategy.manga_strategy import MangaSource
 
 load_dotenv()
 httpx_client = HTTPX()
 
-class Manhwa18Source:    
+class Manhwa18Source(MangaSource):    
     base_url = os.getenv("MANHWA18_BASE_URL")
 
     async def search(self, series_name):
