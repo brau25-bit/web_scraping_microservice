75130c2ac8bb69d3b96cc6e3db28d129d924b4a7
brau25-bit <brauliochaveza@gmail.com>
2026-06-26 16:18:36 -0600

    fix: añadi el campo source al DTO de request

--- src/models/request.py
+++ src/models/request.py
@@ -2,9 +2,11 @@ from pydantic import BaseModel
 
 class MangaRequest(BaseModel):
     series_url: str
+    source: str
 
 class ChapterRequest(BaseModel):
     chapter_url: str
+    source: str
 
 class SearchRequest(BaseModel):
     series_name: str
