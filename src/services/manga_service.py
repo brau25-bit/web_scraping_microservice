db870b8c4567029477f8477fa6723482885328a5
brau25-bit <brauliochaveza@gmail.com>
2026-06-30 23:06:25 -0600

    add: logica de negocio para la producción de mensajes a la cola

--- src/services/manga_service.py
+++ src/services/manga_service.py
@@ -3,6 +3,9 @@ from src.factory.parser_factory import ParserFactory
 from src.models.image import Chapter
 from src.models.search import MangaSearchResult
 from src.models.manga import MangaDetails
+from src.core.rabbitMQ import RabbitClient
+from src.builder.chapter_publisher_builder import ChapterDownloadBuilder
+from src.builder.publisher_builder import SeriesDiscoveredBuilder
 
 async def search_series(series_name: str, source: str) -> MangaSearchResult:
     try:
@@ -37,5 +40,51 @@ async def get_chapter_img(chapter_url: str, source: str) -> Chapter:
         parsedSearchResult = parser.getChapter()
 
         return parsedSearchResult
+    except Exception as e:
+        raise e
+    
+async def publish_download(data):
+    try:
+        rabbit = RabbitClient()
+        await rabbit.connect()
+
+        source = data.source
+        series_id = data.id
+        series_url = data.series.url
+
+        series_info = (
+            SeriesDiscoveredBuilder()
+            .set_source(source)
+            .set_title(data.series.title)
+            .set_cover(data.series.cover)
+            .build()
+        )      
+
+        event = series_info["event"]
+
+        await rabbit.create_queue(event)
+        await rabbit.publish(event, series_info)
+
+        for chapter in data.chapters:
+            chapter_img = await get_chapter_img(chapter.chapter_url, source)
+
+            chapter_number = chapter_img["chapter"]
+
+            chapter_imgs = chapter_img["manga_chapter"]
+
+            chapter_build = (
+                ChapterDownloadBuilder(series_id)
+                .set_chapter_url(series_url)
+                .set_chapter_number(chapter_number)
+                .set_chapter_imgs(chapter_imgs)
+                .build()
+            )
+            
+            event = chapter_build["event"]
+            await rabbit.create_queue(event)
+            await rabbit.publish(event, chapter_build)
+        
+        rabbit.close()
+        return "Finalizado"
     except Exception as e:
         raise e
