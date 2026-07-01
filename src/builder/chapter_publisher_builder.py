ffc6bcb324e918b2fab7e9108a5d67f142094026
brau25-bit <brauliochaveza@gmail.com>
2026-06-30 23:02:51 -0600

    add: builder de objeto para mensaje de descarga de capitulo

--- src/builder/chapter_publisher_builder.py
+++ src/builder/chapter_publisher_builder.py
@@ -2,18 +2,31 @@ class ChapterDownloadBuilder:
 
     def __init__(self, series_id):
         self.data = {
-            "event": "chapter.download.requested",
+            "event": "chapter.download.queue",
             "series_id": series_id
         }
+
+    def set_chapter_number(self, chapter: str):
+        self.data["chapter_number"] = chapter
+        return self
 
     def set_chapter_url(self, chapter: str):
         self.data["chapter_url"] = chapter
         return self
     
-    def set_chapter_number(self, chapter: str):
-        self.data["chapter_number"] = chapter
+    def set_chapter_imgs(self, chapter):
+        self.data["chapter_images"] = chapter ## Espera chapter_img y chapter_url
         return self
     
-    def set_chapter_imgs(self, chapter):
-        self.data["chapters_url"] = chapter ## Espera chapter_img y chapter_url
-        return self
+    def build(self):
+        required = [
+            "chapter_number",
+            "chapter_url",
+            "chapter_images"
+        ]
+
+        for field in required:
+            if field not in self.data:
+                raise ValueError(f"{field} is required")
+            
+        return self.data
